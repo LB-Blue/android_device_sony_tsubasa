a5efbb0225d5c46ddbb7f7fe1b42ee1e988d305b
ISTweak <istweak@d-o-b.org>
2015-10-09 19:15:53 +0900

    modified extract-files.sh
    Change-Id: I4fea4eaa79373c5195bb3c28f19d1b6368d9d24b

--- extract-files.sh
+++ extract-files.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
-# Use tradition sort
-export LC_ALL=C
-
 FP=$(cd ${0%/*} && pwd -P)
 export VENDOR=$(basename $(dirname $FP))
 export DEVICE=$(basename $FP)
 export BOARDCONFIGVENDOR=true
 
-../common/extract-files.sh $@
+mkdir system
+sudo mount -o loop -t ext4 system.ext4 system
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $BASE/*
+
+for FILE in `grep -v ^# proprietary-files.txt | grep -v ^$ | sort`
+do
+  # Split the file from the destination (format is "file[:destination]")
+  OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+  FILE=${PARSING_ARRAY[0]}
+  DEST=${PARSING_ARRAY[1]}
+  if [ -z "$DEST" ]; then
+    DEST=$FILE
+  fi
+  DIR=`dirname $DEST`
+  if [ ! -d $BASE/$DIR ]; then
+    mkdir -p $BASE/$DIR
+  fi
+  
+  cp ./system/$FILE $BASE/$DEST
+  # if file dot not exist try destination
+  if [ "$?" != "0" ]; then
+    cp ./system/$DEST $BASE/$DEST
+  fi
+done
 
 ../common/setup-makefiles.sh
+
+sudo umount system
+rm -rf system
