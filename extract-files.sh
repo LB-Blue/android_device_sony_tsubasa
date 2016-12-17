4a99bad7d3bd67cddf76a56dff10e656e0b3e739
ISTweak <istweak@d-o-b.org>
2016-12-18 08:47:31 +0900

    update gpu driver
    Change-Id: I7de0af4bf0eb045cf160b63f3cafba2bc6528188

--- extract-files.sh
+++ extract-files.sh
@@ -38,3 +38,33 @@ bspatch $BASE/vendor/lib/libqc-opt.so $BASE/vendor/lib/libqc-opt.so libqc-opt.bs
 
 sudo umount system
 rm -rf system
+
+func_wget() {
+	FN=$1
+	DR=$2
+	URL=https://github.com/TheMuppets/proprietary_vendor_sony/raw/cm-13.0/blue-common/proprietary/vendor/lib/
+	wget -O $BASE/vendor/lib/$DR$FN $URL$DR$FN
+}
+
+if [ ! -d $BASE/vendor/lib/egl ]; then
+	mkdir -p $BASE/vendor/lib/egl
+fi
+
+func_wget eglsubAndroid.so egl/
+func_wget libEGL_adreno.so egl/
+func_wget libGLESv1_CM_adreno.so egl/
+func_wget libGLESv2_adreno.so egl/
+func_wget libGLESv2S3D_adreno.so egl/
+func_wget libq3dtools_adreno.so egl/
+func_wget libadreno_utils.so
+func_wget libC2D2.so
+func_wget libc2d2_a3xx.so
+func_wget libc2d2_z180.so
+func_wget libCB.so
+func_wget libgsl.so
+func_wget libllvm-a3xx.so
+func_wget libllvm-arm.so
+func_wget libOpenCL.so
+func_wget libOpenVG.so
+func_wget libsc-a2xx.so
+func_wget libsc-a3xx.so
